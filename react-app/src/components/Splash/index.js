a446bc9f6aae3560cf4bd7aee5fe2a893114a212
Zane Preudhomme <zane@effarig.com>
2021-05-24 07:32:01 -0700

    Splash page styling pretty set, finished all sections, buttons, navbars, may just need to adjust content

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -12,6 +12,8 @@ import flask from "../../images/technologies/flask_logo.png"
 import postgres from "../../images/technologies/postgres_logo.png"
 import react from "../../images/technologies/react_logo.png"
 import docker from "../../images/technologies/docker_logo.png"
+import slack_screenshot from "../../images/slack_clone_screenshot.png"
+import slack_logo from "../../images/slack_logo.png"
 
 
 const SlideUp = batch(MoveIn(0,1000),MoveOut(0, -200), Sticky(), FadeOut(1, 0.8))
@@ -30,6 +32,13 @@ function Splash(){
     })
   }
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
   const onScroll = () => {
     if (window.scrollY >= 1950){
       document.getElementById("scroll-btn").onclick = scrollToPage3
@@ -41,8 +50,13 @@ function Splash(){
       document.getElementById("splash_container").classList.add("splash_container-middle")
       document.getElementById("splash_container").classList.remove("splash_container-last")
     }
-    if(window.scrollY < 1000){
+    if (window.scrollY > 1000){
+      document.getElementById("splash_scrolling_nav").classList.remove("hidden")
+    }
+    if(window.scrollY <= 1000){
       document.getElementById("splash_container").classList.remove("splash_container-middle")
+      document.getElementById("splash_scrolling_nav").classList.add("hidden")
+      
     }
     if(window.scrollY > 2900){
       document.getElementById("splash_container").classList.add("splash_container-last")
@@ -66,17 +80,23 @@ function Splash(){
   return (
     <div className="splash_container" id="splash_container">
       <nav className="splash_nav">
-        <h1 className="nav_title">slack</h1>
+        <div className="splash_nav-left">
+          <img src={slack_logo} alt="logo" id="slack_logo"/>
+          <h1 className="nav_title">slack</h1>
+        </div>
         <div className="splash_nav-right">
           <Link to="/login"><div className="splash_nav-link signin_btn">Sign In</div></Link>
           <Link to="/sign-up"><div className="splash_nav-link tryout_btn">Try for free</div></Link>
         </div>
       </nav>
-      <nav className="splash_scrolling_nav">
-        <h1 className="splash_scolling_nav_title">slack</h1>
+      <nav className="splash_scrolling_nav hidden" id="splash_scrolling_nav">
+        <div className="splash_scrolling_nav-left" onClick={scrollToTop}>
+          <img src={slack_logo} alt="logo" id="slack_logo"/>
+          <h1 className="splash_scolling_nav_title">slack</h1>
+        </div>
         <div className="splash_scrolling_nav-right">
         <Link to="/login"><div className="splash_scrolling_signin">Sign In</div></Link>
-          <Link to="/sign-up"><div className="splash_scrolling_tryout">Try for free</div></Link>
+        <Link to="/sign-up"><div className="splash_scrolling_tryout">Try for free</div></Link>
         </div>
       </nav>
       <button className="scroll-btn" id="scroll-btn">Continue</button>
@@ -89,7 +109,7 @@ function Splash(){
                 ! Want to check it out?</h1>
               <Link to="/sign-up"><div className="splash_tryout_btn">Try for free</div></Link>
               {/*Replace image with something more appropriate*/}
-              <img className="splash_img" src="https://i.pcmag.com/imagery/reviews/07td46ju7p6lLVb0QGwc5VF-6.1569479844.fit_scale.size_1028x578.jpg" alt="slack" />
+              <img className="splash_img" src={slack_screenshot} alt="slack" />
             </div>
           </Animator>
         </ScrollPage>
@@ -103,15 +123,19 @@ function Splash(){
           <Animator animation={SlideUp}>
             <div className="splash_main-2 splash_page">
               <div className="splash_main-2-left">
-                <h2>This project was build with some fun technologies</h2>
+                <h2>Technologies used to create this site</h2>
+                <br/><br/>
+                <h4>This project combines the power of a React frontend to dynamically display
+                  content with a Python backend to serve data and host websockets
+                </h4>
               </div>
               <div className="splash_main-2-right">
-                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" id="js_logo"><img src={js} class="splash-logo" alt="js-logo"/></a>
-                <a href="https://docs.python.org/3/" id="python_logo"><img src={python} class="splash-logo" alt="python-logo"/></a>
-                <a href="https://docs.docker.com/" id="docker_logo"><img src={docker} class="splash-logo" alt="docker-logo"/></a>
-                <a href="https://reactjs.org/" id="react_logo"><img src={react} class="splash-logo" alt="react-logo"/></a>
-                <a href="https://flask.palletsprojects.com/en/2.0.x/" id="flask_logo"><img src={flask} class="splash-logo" alt="flask-logo"/></a>
-                <a href="https://www.postgresql.org/" id="postgres_logo"><img src={postgres} class="splash-logo" alt="postgres-logo"/></a>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" id="js_logo"><img src={js} className="splash-logo" alt="js-logo"/></a>
+                <a href="https://docs.python.org/3/" id="python_logo"><img src={python} className="splash-logo" alt="python-logo"/></a>
+                <a href="https://docs.docker.com/" id="docker_logo"><img src={docker} className="splash-logo" alt="docker-logo"/></a>
+                <a href="https://reactjs.org/" id="react_logo"><img src={react} className="splash-logo" alt="react-logo"/></a>
+                <a href="https://flask.palletsprojects.com/en/2.0.x/" id="flask_logo"><img src={flask} className="splash-logo" alt="flask-logo"/></a>
+                <a href="https://www.postgresql.org/" id="postgres_logo"><img src={postgres} className="splash-logo" alt="postgres-logo"/></a>
               </div>
             </div>
           </Animator>
