c92f33c70e45b5bc5930ce011df5f2b022ab3e90
brentarimoto <brentarimoto@gmail.com>
2021-05-25 14:34:41 -0700

    setup for notifications

--- react-app/src/components/AppUI/SideBar/Channels.js
+++ react-app/src/components/AppUI/SideBar/Channels.js
@@ -4,6 +4,27 @@ import { useSelector } from 'react-redux';
 import './Channels.css';
 import { NavLink } from 'react-router-dom';
 
+const Nav = ({channel, setRoom}) =>{
+	const [isClicked, setIsClicked] = useState(false)
+
+	let handleClick = ()=>{
+		if (!isClicked){
+			dispatch
+			setIsClicked(true)
+		}
+		setRoom(`Channel: ${channel.id}`)
+	}
+
+	return(
+		<li key={channel.id} className="channels__button">
+			<NavLink onClick={handleClick} to={`/channels/${channel.id}`}>
+				<span>{channel.name}</span>
+			</NavLink>
+		</li>
+	)
+}
+
+
 const Channels = ({ setRoom }) => {
 	const channels = useSelector(state => state.channels);
 
@@ -47,6 +68,9 @@ const Channels = ({ setRoom }) => {
 						<span>2021-01-group02-juice-and-the-thunks</span>
 					</button>
 				</li>
+				{arr?.map(channel=>{
+					<Nav channel={channel} setRoom={setRoom}/>
+				})}
 				{channelComponents}
 				<li className="channels__item">
 					<button className="channels__add">
