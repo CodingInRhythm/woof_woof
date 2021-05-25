9ef0a3c1cebe1fb84d8f56ee18e9fdc6e16d52f1
brentarimoto <brentarimoto@gmail.com>
2021-05-25 09:32:12 -0700

    added channel messages store

---
+++ react-app/src/store/channel_messages.js
@@ -0,0 +1,54 @@
+/*************************** OTHER FILE IMPORTS ***************************/
+
+/*************************** TYPES ***************************/
+const SET_CHANNEL_MESSAGES = "set/CHANNEL_MESSAGES";
+
+const ADD_CHANNEL_MESSAGE = "add/CHANNEL_MESSAGE";
+
+
+/*************************** ACTIONS ***************************/
+const setMessages = (channel_id, messages) => ({
+    type: SET_CHANNEL_MESSAGES,
+    channel_id,
+    messages,
+});
+
+export const addMessage = (channel_id, message)=> ({
+    type: ADD_CHANNEL_MESSAGE,
+    channel_id,
+    message
+});
+
+/*************************** THUNKS ***************************/
+export const getChannelMessages = (channel_id) => async (dispatch) => {
+    const response = await fetch(`/api/messages/channel/${channel_id}`);
+
+    const data = await response.json();
+
+    if (data.errors) {
+        return;
+    }
+
+    dispatch(setMessages(channel_id, data['channel_messages']))
+}
+
+
+/*************************** REDUCER ***************************/
+
+const initialState = {};
+
+export default function channelMessageReducer(state=initialState, action) {
+    let newState;
+    switch (action.type) {
+        case SET_CHANNEL_MESSAGES:
+            newState = {...state}
+            newState[action.channel_id]=action.messages
+            return newState
+        case ADD_CHANNEL_MESSAGE:
+            newState = {...state}
+            newState[action.channel_id]=[...newState[action.channel_id], action.message]
+            return newState
+        default:
+            return state;
+    }
+}
