5cfb0ece9b041cce45c2867546aa47e3bd587cb7
Alex Clough <alexcl123@gmail.com>
2021-05-31 14:33:34 -0400

    css adjustments in loginform and signupform pages.  Bug with formatting on 'photo upload form'

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -61,73 +61,88 @@ const SignUpForm = () => {
 	}
 
 	return (
-		<>
-			<div className="login-container">
-				<div></div>
-				<div className="loginWrap">
-					<img src={logo} className="slack_logo" onClick={logoClick} alt="logo"></img>
-					<h1 className="login-form-header">Join Woof Woof!</h1>
+    <>
+      <div className="login-container">
+        <div></div>
+        <div className="loginWrap">
+          {/* <img
+            src={logo}
+            className="slack_logo"
+            onClick={logoClick}
+            alt="logo"
+          ></img> */}
+          <i
+            onClick={logoClick}
+            id="fa-dog_green-large_login"
+            class="fas fa-dog"
+          ></i>
+          <h1 className="login-form-header">Join Woof Woof!</h1>
 
-					<form onSubmit={onSignUp}>
-						<div>
-							<input
-								name="username"
-								id="username"
-								type="text"
-								required
-								onChange={updateUsername}
-								placeholder="Username"
-								className="login-input"
-								value={username}
-							/>
-						</div>
-						<div>
-							<input
-								name="firstName"
-								id="firstName"
-								type="text"
-								required
-								onChange={updateFirstName}
-								placeholder="First Name"
-								className="login-input"
-								value={firstName}
-							/>
-						</div>
-						<div>
-							<input
-								name="lastName"
-								id="lastName"
-								type="text"
-								required
-								onChange={updateLastName}
-								placeholder="Last Name"
-								className="login-input"
-								value={lastName}
-							/>
-						</div>
-						<div>
-							<input
-								id="email"
-								type="email"
-								required
-								onChange={updateEmail}
-								placeholder="Email"
-								className="login-input"
-								value={email}
-							/>
-						</div>
+          <form onSubmit={onSignUp}>
+            <div>
+              <input
+                name="username"
+                id="username"
+                type="text"
+                required
+                onChange={updateUsername}
+                placeholder="Username"
+                className="login-input"
+                value={username}
+              />
+            </div>
+            <div>
+              <input
+                name="firstName"
+                id="firstName"
+                type="text"
+                required
+                onChange={updateFirstName}
+                placeholder="First Name"
+                className="login-input"
+                value={firstName}
+              />
+            </div>
+            <div>
+              <input
+                name="lastName"
+                id="lastName"
+                type="text"
+                required
+                onChange={updateLastName}
+                placeholder="Last Name"
+                className="login-input"
+                value={lastName}
+              />
+            </div>
+            <div>
+              <input
+                id="email"
+                type="email"
+                required
+                onChange={updateEmail}
+                placeholder="Email"
+                className="login-input"
+                value={email}
+              />
+            </div>
 
-						<div className="photo-upload--form">
-							<span id="file-chosen" className="photo-upload--filename">
-								{photo ? `${photo.name}` : 'No file chosen'}
-							</span>
-							<input type="file" id="actual-btn" onChange={updatePhoto} hidden />
+            <div className="photo-upload--form">
+              <span id="file-chosen" className="photo-upload--filename">
+                {photo ? `${photo.name}` : "No file chosen"}
+              </span>
+              <input
+                type="file"
+                id="actual-btn"
+                onChange={updatePhoto}
+                hidden
+              />
 
-							<label for="actual-btn" className="photo-upload--btn">
-								Choose File
-							</label>
+              <label for="actual-btn" className="photo-upload--btn">
+                <span id="actual-btn-text">Choose File</span>
+              </label>
 
-							{/* <input
+              {/* <input
 								id="photo-btn"
 								type="input"
 								onChange={updatePhoto}
@@ -138,44 +153,44 @@ const SignUpForm = () => {
 							<button className="photo-upload--btn">
 								<i class="fas fa-file-upload"></i>
 							</button> */}
-						</div>
-						<div>
-							<input
-								id="password"
-								type="password"
-								value={password}
-								onChange={updatePassword}
-								required
-								placeholder="Password"
-								className="login-input"
-							/>
-						</div>
-						<div>
-							<input
-								id="password"
-								type="password"
-								name="repeat_password"
-								onChange={updateRepeatPassword}
-								value={repeatPassword}
-								required={true}
-								placeholder="Password Confirmation"
-								className="login-input"
-							/>
-						</div>
-						<button type="submit" id="submitButton" className="login-btn">
-							Sign Up
-						</button>
-					</form>
-				</div>
-				<div className="signup-link">
-					<p className="switchLinkName">Already using Slack?</p>
-					<NavLink to="/login" className="switchLink">
-						Sign in here
-					</NavLink>
-				</div>
-			</div>
-		</>
-	);
+            </div>
+            <div>
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={updatePassword}
+                required
+                placeholder="Password"
+                className="login-input"
+              />
+            </div>
+            <div>
+              <input
+                id="password"
+                type="password"
+                name="repeat_password"
+                onChange={updateRepeatPassword}
+                value={repeatPassword}
+                required={true}
+                placeholder="Password Confirmation"
+                className="login-input"
+              />
+            </div>
+            <button type="submit" id="submitButton" className="login-btn">
+              Sign Up
+            </button>
+          </form>
+        </div>
+        <div className="signup-link">
+          <p className="switchLinkName">Already using Slack?</p>
+          <NavLink to="/login" className="switchLink">
+            Sign in here
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUpForm;
