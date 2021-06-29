b8cab8ab3c3b8a585353f7eba3e3671acb87a308
brentarimoto <brentarimoto@gmail.com>
2021-06-29 13:01:42 -0700

    updated the linkedin links

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -250,13 +250,13 @@ function Splash(){
               <div className="splash_main-3-creator" id="creator-nurs">
                 <img src={slack_nurs} alt="nurs-profile"></img>
                 <a href='https://nasanov.github.io/' target="_blank" rel="noopener noreferrer"><h3>Nurs Asanov</h3></a>
-                <a href="https://www.linkedin.com/in/brent-arimoto/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/nursultan-asanov/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a href="https://github.com/nasanov" target="_blank" rel="noopener noreferrer">GitHub</a>
               </div>
               <div className="splash_main-3-creator" id="creator-alex">
                 <img src={github_alex} alt="alex-profile"></img>
                 <h3>Alex Clough</h3>
-                <a href="https://www.linkedin.com/in/brent-arimoto/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/alex-clough-710546200/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a href="https://github.com/CodingInRhythm" target="_blank" rel="noopener noreferrer">GitHub</a>
               </div>
               <div className="splash_main-3-creator" id="creator-brent">
