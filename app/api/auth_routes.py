798e45440aa3d3e554b33a94c72a04584ab37812
brentarimoto <brentarimoto@gmail.com>
2021-07-01 18:35:17 -0700

    aws fixing

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -165,11 +165,11 @@ def edit_user(id):
         if form.data['lastname'] and form.data['lastname']!= user.lastname:
             user.lastname=form.data['lastname']
         if url and url!= user.profile_photo:
-            if 'amazonaws' in user.profile_photo:
-                key=user.profile_photo.split('/')[-1]
-                res = delete_file_from_s3(key)
-                if 'success' not in res:
-                    return res, 400
+            if user.profile_photo and ('amazonaws' in user.profile_photo):
+                    key=user.profile_photo.split('/')[-1]
+                    res = delete_file_from_s3(key)
+                    if 'success' not in res:
+                        return res, 400
             user.profile_photo=url
         db.session.commit()
         login_user(user)
